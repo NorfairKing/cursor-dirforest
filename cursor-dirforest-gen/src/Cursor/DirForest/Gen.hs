5f4c729e16b84fa81c87bc909d7fe9d27adeef78
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-22 13:47:58 +0200

    Sped up the generators by an order of magnitured

--- cursor-dirforest-gen/src/Cursor/DirForest/Gen.hs
+++ cursor-dirforest-gen/src/Cursor/DirForest/Gen.hs
@@ -1,18 +1,54 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cursor.DirForest.Gen where
 
 import Cursor.DirForest
+import Cursor.DirForest
 import Cursor.Map.Gen
-import Data.DirForest (DirForest)
+import Data.DirForest (DirForest (..), DirTree (..))
+import qualified Data.DirForest as DF
 import Data.GenValidity
 import Data.GenValidity.Containers
 import Data.GenValidity.DirForest
+import Path
+import qualified System.FilePath as FP
+import Test.QuickCheck
 
 instance (GenValid a, Ord a) => GenValid (DirForestCursor a) where
-  genValid = genValidStructurally
   shrinkValid = shrinkValidStructurally
+  genValid = DirForestCursor <$> genMapCursorByDependent go1 go2 go3
+    where
+      isTopLevel p_ = parent p_ == [reldir|./|]
+      go3 :: Gen (FilePath, DirTree a)
+      go3 = go1
+      go1 :: Gen (FilePath, DirTree a)
+      go1 = do
+        dt <-
+          genValid
+            `suchThat` ( \case
+                           NodeDir df -> not $ DF.null df
+                           _ -> True
+                       )
+        fp <- case dt of
+          NodeFile _ -> fromRelFile <$> (genValid `suchThat` isTopLevel)
+          NodeDir _ -> (FP.dropTrailingPathSeparator . fromRelDir) <$> (genValid `suchThat` isTopLevel)
+        pure (fp, dt)
+      go2 :: Gen (FilePath, DirTreeCursor a)
+      go2 = do
+        dtc <- genValid
+        fp <- case dtc of
+          DirTreeCursorFile _ -> fromRelFile <$> (genValid `suchThat` isTopLevel)
+          DirTreeCursorDir _ -> (FP.dropTrailingPathSeparator . fromRelDir) <$> (genValid `suchThat` isTopLevel)
+        pure (fp, dtc)
 
 instance (GenValid a, Ord a) => GenValid (DirTreeCursor a) where
-  genValid = genValidStructurally
   shrinkValid = shrinkValidStructurally
+  genValid = sized $ \n ->
+    scale (\x -> max x $ x - 1) $
+      oneof
+        [ DirTreeCursorFile <$> genValid,
+          DirTreeCursorDir <$> genValid
+        ]
