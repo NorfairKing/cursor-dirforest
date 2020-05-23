6e82161718c963085ab2fa8da8733248103b86c2
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-23 16:44:52 +0200

    An actual cursor that you can jump around in

--- cursor-dirforest-brick/src/Cursor/DirForest/Brick.hs
+++ cursor-dirforest-brick/src/Cursor/DirForest/Brick.hs
@@ -2,7 +2,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Cursor.DirForest.Brick where
+module Cursor.DirForest.Brick
+  ( verticalDirForestCursorWidget,
+    dirForestCursorWidget,
+    dirTreeCursorWidget,
+  )
+where
 
 import Brick.AttrMap
 import Brick.Main
@@ -17,27 +22,22 @@ import Data.Int
 import qualified Data.Map as M
 import Graphics.Vty.Input.Events
 
-drawDirForestCursor :: forall a n. (a -> Widget n) -> DirForestCursor a -> Widget n
-drawDirForestCursor func = verticalMapCursorWidget (drawDirTreePair func) goKVC (drawDirTreePair func) . dirForestCursorMapCursor
-  where
-    goKVC :: KeyValueCursor FilePath (DirTreeCursor a) FilePath (DirTree a) -> Widget n
-    goKVC = keyValueWidget (drawDirTreePair func) (drawDirTreeCursorPair func)
+verticalDirForestCursorWidget ::
+  (FilePath -> DirTree a -> Widget n) ->
+  (KeyValueCursor FilePath (DirTreeCursor a) FilePath (DirTree a) -> Widget n) ->
+  (FilePath -> DirTree a -> Widget n) ->
+  DirForestCursor a ->
+  Widget n
+verticalDirForestCursorWidget beforeFunc currentFunc afterFunc = dirForestCursorWidget $ \befores current afters ->
+  vBox $
+    concat
+      [ map (uncurry beforeFunc) befores,
+        [currentFunc current],
+        map (uncurry afterFunc) afters
+      ]
 
-drawDirTreePair :: (a -> Widget n) -> FilePath -> DirTree a -> Widget n
-drawDirTreePair func fp dt = vBox [str fp, padLeft (Pad 2) $ drawDirTree func dt]
+dirForestCursorWidget :: ([(FilePath, DirTree a)] -> KeyValueCursor FilePath (DirTreeCursor a) FilePath (DirTree a) -> [(FilePath, DirTree a)] -> Widget n) -> DirForestCursor a -> Widget n
+dirForestCursorWidget = foldDirForestCursor
 
-drawDirTreeCursorPair :: (a -> Widget n) -> FilePath -> DirTreeCursor a -> Widget n
-drawDirTreeCursorPair func fp dtc = vBox [str fp, padLeft (Pad 2) $ drawDirTreeCursor func dtc]
-
-drawDirTree :: (a -> Widget n) -> DirTree a -> Widget n
-drawDirTree func = \case
-  NodeFile a -> func a
-  NodeDir df -> drawDirForest func df
-
-drawDirForest :: (a -> Widget n) -> DirForest a -> Widget n
-drawDirForest func (DirForest m) = vBox $ map (uncurry $ drawDirTreePair func) $ M.toList m
-
-drawDirTreeCursor :: (a -> Widget n) -> DirTreeCursor a -> Widget n
-drawDirTreeCursor func = \case
-  DirTreeCursorFile a -> func a
-  DirTreeCursorDir mdfc -> maybe emptyWidget (drawDirForestCursor func) mdfc
+dirTreeCursorWidget :: (a -> Widget n) -> (Maybe (DirForestCursor a) -> Widget n) -> (DirTreeCursor a -> Widget n)
+dirTreeCursorWidget = foldDirTreeCursor
--- cursor-dirforest-demo/src/Tui.hs
+++ cursor-dirforest-demo/src/Tui.hs
@@ -7,6 +7,7 @@ module Tui where
 import Brick.AttrMap
 import Brick.Main
 import Brick.Types
+import Brick.Util
 import Brick.Widgets.Core
 import Cursor.Brick
 import Cursor.DirForest
@@ -16,17 +17,20 @@ import qualified Data.DirForest as DF
 import Data.DirForest (DirForest (..), DirTree (..))
 import Data.Int
 import qualified Data.Map as M
+import Data.Maybe
+import Graphics.Vty.Attributes
 import Graphics.Vty.Input.Events
 import Path
 import Path.IO
 import System.Directory
 import System.Posix
+import Text.Show.Pretty
 
 cursorDirForestDemo :: IO ()
 cursorDirForestDemo = do
   initialState <- buildInitialState
   endState <- defaultMain tuiApp initialState
-  print endState
+  pPrint endState
 
 data TuiState
   = TuiState {stateCursor :: Maybe (DirForestCursor Int64)}
@@ -43,8 +47,11 @@ tuiApp =
       appChooseCursor = showFirstCursor,
       appHandleEvent = handleTuiEvent,
       appStartEvent = pure,
-      appAttrMap = const $ attrMap mempty []
+      appAttrMap = const $ attrMap defAttr [(selectedAttr, fg white)]
     }
+
+selectedAttr :: AttrName
+selectedAttr = "selected"
 
 buildInitialState :: IO TuiState
 buildInitialState = do
@@ -54,14 +61,55 @@ buildInitialState = do
     pure size
   pure $ TuiState $ makeDirForestCursor df
 
-drawTui :: TuiState -> [Widget ResourceName]
-drawTui ts = [maybe emptyWidget (drawDirForestCursor $ \i -> str (show i) <+> str " bytes") (stateCursor ts)]
+drawTui :: TuiState -> [Widget n]
+drawTui ts = [maybe emptyWidget drawDirForestInt64Cursor (stateCursor ts)]
+
+drawDirForestInt64Cursor :: DirForestCursor Int64 -> Widget n
+drawDirForestInt64Cursor =
+  verticalDirForestCursorWidget
+    goDirTreePair
+    goKVC
+    goDirTreePair
+  where
+    goDirTreePair :: FilePath -> DirTree Int64 -> Widget n
+    goDirTreePair fp = \case
+      NodeFile a -> str fp <+> str " " <+> goInt64 a
+      NodeDir df -> str fp <=> padLeft (Pad 2) (drawDirForest df)
+    drawDirForest :: DirForest Int64 -> Widget n
+    drawDirForest (DirForest m) = vBox $ map (uncurry goDirTreePair) $ M.toList m
+    goDirTreeCursorPair :: FilePath -> DirTreeCursor Int64 -> Widget n
+    goDirTreeCursorPair fp =
+      withAttr selectedAttr
+        . dirTreeCursorWidget
+          (\a -> str fp <+> str " " <+> goInt64 a)
+          ( \mdf -> case mdf of
+              Nothing -> str fp
+              Just df -> str fp <=> drawDirForestInt64Cursor df
+          )
+    goKeySelected fp = \case
+      NodeFile a -> withAttr selectedAttr (str fp) <+> str " " <+> goInt64 a
+      NodeDir df -> withAttr selectedAttr (str fp) <=> padLeft (Pad 2) (drawDirForest df)
+    goValueSelected fp dtc =
+      withAttr selectedAttr $ goDirTreeCursorPair fp dtc
+    goKVC = keyValueWidget goKeySelected goValueSelected
+    goInt64 i = str (show i) <+> str " bytes"
 
 handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
 handleTuiEvent s e =
   case e of
     VtyEvent vtye ->
-      case vtye of
-        EvKey (KChar 'q') [] -> halt s
-        _ -> continue s
+      let doP func =
+            continue $
+              s
+                { stateCursor = case stateCursor s of
+                    Nothing -> Nothing
+                    Just dfc -> Just $ func dfc
+                }
+          doM func = doP (\c -> fromMaybe c $ func c)
+       in case vtye of
+            EvKey (KChar 'q') [] -> halt s
+            EvKey (KChar 'j') [] -> doM dirForestCursorSelectNextOnSameLevel
+            EvKey KDown [] -> doM dirForestCursorSelectNextOnSameLevel
+            EvKey KUp [] -> doM dirForestCursorSelectPrevOnSameLevel
+            _ -> continue s
     _ -> continue s
